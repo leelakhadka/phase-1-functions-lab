d6e26dc59cc99d8bff734502709bfc5f554ccc43
leela khadka <khadkaleela3@gmail.com>
2022-08-08 22:37:03 -0700

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+
+    if (distance < 42) {
+        return 42 - distance;
+    }
+    return distance - 42;
+
+}
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination - start > 0) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    var distance = distanceTravelledInFeet(start, destination);
+
+    if (distance < 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+
+}
